0999ea1cd74caf81fcb3ac6092fbc5a8b2fbaccf
Mostafa Ehab <Mostafa-Ehab>
2023-06-22 19:05:45 +0300

    Add Admin add book page

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+// tinymce.init({
+//     selector: 'textarea',  // change this value according to your HTML
+//     plugins: 'code wordcount directionality',
+//     // toolbar: 'ltr rtl code',
+//     a_plugin_option: true,
+//     a_configuration_option: 400
+// });
+
+tinymce.init({
+    selector: 'textarea',
+    // plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+    // toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    // tinycomments_mode: 'embedded',
+    // tinycomments_author: 'Author name',
+    // mergetags_list: [
+    //     { value: 'First.Name', title: 'First Name' },
+    //     { value: 'Email', title: 'Email' },
+    // ]
+});
+
