a7e231d5774152fa128834e352c509c80b9b8447
Mostafa Ehab <Mostafa-Ehab>
2023-06-23 17:49:32 +0300

    Finish book page design - Add media query files

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("")
+    return render_template("users/index.html")
+
+@app.route("/book/<sid>")
+def book(sid):
+    print(sid)
+    return render_template("users/book.html", title="Hello world!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
 
 
