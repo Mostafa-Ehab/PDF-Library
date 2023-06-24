4c4832868c273a3fe347a3a9dfed19481019235b
Mostafa Ehab <Mostafa-Ehab>
2023-06-24 04:12:30 +0300

    Add history, wishkist and login design

--- app.py
+++ app.py
@@ -11,6 +11,19 @@ def book(sid):
     print(sid)
     return render_template("users/book.html", title="Hello world!")
 
+@app.route("/history")
+def history():
+    return render_template("users/history.html")
+
+
+@app.route("/wishlist")
+def wishlist():
+    return render_template("users/wishlist.html")
+
+@app.route("/login")
+def login():
+    return render_template("users/login.html", del_sec_side_bar=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
---
+++ static/js/layout.js
@@ -0,0 +1,51 @@
+/*
+    Control Menus Close and Open Buttons
+*/
+let mainSideBar = document.querySelector(".main-side-bar")
+let mainSection = document.querySelector(".main")
+let secSideBar = document.querySelector(".sec-side-bar")
+
+// /* Close Main Side Bar */
+// document.querySelector("#close-main").addEventListener("click", (event) => {
+//     mainSideBar.style.left = "-200px"
+//     mainSection.style.left = "10px"
+
+//     document.querySelector("#open-main").style.left = "0";
+// })
+
+// /* Open Main Side Bar */
+// document.querySelector("#open-main").addEventListener("click", (event) => {
+//     mainSideBar.style.left = "10px"
+//     mainSection.style.left = "220px"
+
+//     document.querySelector("#open-main").style.left = "-100px";
+// })
+
+/* Close Secondary Side Bar */
+document.querySelector("#close-sec").addEventListener("click", (event) => {
+    secSideBar.style.right = "-400px"
+    mainSection.style.right = "10px"
+
+    document.querySelector("#open-sec").style.right = "0";
+})
+
+/* Open Secondary Side Bar */
+document.querySelector("#open-sec").addEventListener("click", (event) => {
+    secSideBar.style.right = "10px"
+    mainSection.style.right = "420px"
+
+    document.querySelector("#open-sec").style.right = "-100px";
+})
+
+/*
+    Delete Secondary Side Bar in Login/Register Pages
+*/
+// document.addEventListener("load", (event) => {
+
+if (secSideBar.getAttribute("data-del")) {
+    console.log("not found")
+    secSideBar.style.right = "-400px";
+    mainSection.style.right = "10px";
+}
+
+
